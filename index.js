3d8dc76c40fe778e0289ff675f6540332f727c3d
Breno Calazans <brenocalazans@gmail.com>
2018-12-10 14:40:06 -0200

    Init commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+
+function getGitVersion() {
+  return exec('git describe --exact-match HEAD').then(response => {
+    const version = response.stdout.replace(/v|\n/gi, '')
+    return version
+  })
+}
+
+function getReferenceFile(fileName) {
+  const filePath = path.join(process.cwd(), fileName)
+  const file = require(filePath)
+  return file
+}
+
+function matchGitTag(referenceFileName) {
+  const file = getReferenceFile(referenceFileName)
+  const referenceVersion = file.version
+
+  return getGitVersion().then(gitVersion => {
+
+    return {
+      gitVersion: gitVersion,
+      referenceVersion: referenceVersion,
+      match: gitVersion === referenceVersion,
+    }
+  })
+}
+
+module.exports = matchGitTag
